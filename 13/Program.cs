8024e2517520d94f6b26efe0c8b3c9faac464d8d
unknown <maxmin05@mail.ru>
2022-12-26 12:08:05 +0500

    Задача номер 13

---
+++ 13/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Введите любое трехзначное число.");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstnumber = number /100;
+
+if (firstnumber == 0)
+{
+System.Console.WriteLine("Ваше не трехзначное");
+}
+
+else
+{
+  int secondnumber = number % 10;
+
+  System.Console.WriteLine(secondnumber);  
+}
