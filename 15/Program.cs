acdec5fdc55dd8fea53bbb3ac5e00f9679c2402e
unknown <maxmin05@mail.ru>
2022-12-26 12:58:48 +0500

    Задача номер 15

---
+++ 15/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Введите день недели от 1 до 7 ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7)
+
+{
+    System.Console.WriteLine($"{number} Это число не является  днём недели");
+}
+
+if (number == 6 || number == 7)
+{
+    System.Console.WriteLine($"{number} Это число  является выходным днём недели");
+} 
+
+if (number >= 1) 
+if ( number <= 5) 
+{
+    System.Console.WriteLine($"{number} Это число не является выходным днём недели");
+}
+
+
+
