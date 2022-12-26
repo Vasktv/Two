2f30ed5f5aceb1a8f91adffb819001a0bca85c79
unknown <maxmin05@mail.ru>
2022-12-26 12:27:00 +0500

    Добавили информацию, если в числе больше 3-х цифр

--- 13/Program.cs
+++ 13/Program.cs
@@ -1,18 +1,19 @@
 ﻿
-Console.WriteLine("Введите любое трехзначное число.");
+Console.WriteLine("Введите любое число.");
 
 int number = Convert.ToInt32(Console.ReadLine());
 
 int firstnumber = number /100;
 
-if (firstnumber == 0)
+if (number /100 == 0)
 {
 System.Console.WriteLine("Ваше число не является техзначным.");
+return;
 }
 
-else
+while (number > 999)
 {
-  int secondnumber = number % 10;
-
-  System.Console.WriteLine(secondnumber);  
+  number /= 10;
 }
+
+System.Console.WriteLine(number % 10);
