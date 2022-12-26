8c458956b1b7dee7092d08a399007ec34867b47e
unknown <maxmin05@mail.ru>
2022-12-26 11:42:19 +0500

    Задача 10

---
+++ 10/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите трехзначное число.");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number / 10;
+
+int secondnumber = number2 % 10;
+
+System.Console.WriteLine(secondnumber);
+
+
